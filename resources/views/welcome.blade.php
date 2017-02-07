007e1592401c63520fa90a0ce6f95ccce1306027
Kenan <xinkenan.11@hotmail.com>
2017-02-07 13:43:51 +0800

    modify welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,7 +70,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Meet BillOrg
+                    Meet Bil
                 </div>
             </div>
         </div>
