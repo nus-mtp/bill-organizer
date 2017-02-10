59caa1e8d95bf5a8f3d7d6f9bdfb66eb5e016af9
Kenan <xinkenan.11@hotmail.com>
2017-02-10 15:28:31 +0800

    test deployment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,9 +33,9 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home1</a>
+                        <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/login') }}">Login1</a>
                         <a href="{{ url('/register') }}">Register</a>
                     @endif
                 </div>
