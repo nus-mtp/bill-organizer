66b0cd74b470504cdf1b312cc7918a12cf3d293c
Kenan <xinkenan.11@hotmail.com>
2017-02-10 15:25:41 +0800

    test circle ci deployment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,20 +4,20 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+
         <title>BillOrganiser</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" type="text/css">
-        
+
         <!-- Stylesheets -->
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.css">
         <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
-        
+
     </head>
     <body>
-        
+
         <div class="ui top fixed menu" style="background:none; border:none;">
             <div class="right menu">
                 <div class="item">
@@ -28,12 +28,12 @@
                 </div>
             </div>
         </div>
-        
+
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Home1</a>
                     @else
                         <a href="{{ url('/login') }}">Login</a>
                         <a href="{{ url('/register') }}">Register</a>
