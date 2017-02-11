462e419b7771c5c130b72f81cfa00deaa1cd4eae
dev-seahouse <xinkenan.11@hotmail.com>
2017-02-11 09:22:18 +0800

    fix semantic import

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>BillOrganiser</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" type="text/css">
