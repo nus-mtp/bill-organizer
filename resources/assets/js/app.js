de047b0fd47dfa30f6ddd8edc1c87d894dc672c1
Yan Ling <A0114391@nus.edu.sg>
2017-02-12 16:09:02 +0800

    linked semantic.js instead of bootstrap.....but why is modal not working???

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,7 +5,7 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap')
+require('./semantic')
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
