f080903aaa4c9d9fe8bb4bce4d0be07a339c3b93
Yan Ling <A0114391@nus.edu.sg>
2017-02-12 13:21:36 +0800

    figured out routing, set basic look of dashboard

--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -1,7 +1,19 @@
+<?php
+// commenting this out first because templating is too messy rn
+
+/*
 @extends('dashboard')
+
+@section('title', 'Dashboard')
+
+@section('topmenu')
+
+    @parent
 
 @section('content')
 
     <h1>Dashboard</h1>
 
 @stop
+*/
+?>
