993688776e97d5c300e64ebf906f74fb782704dd
Kenan <xinkenan.11@hotmail.com>
2017-02-15 01:12:49 +0800

    remove require semantic
    our global library functions should be loaded inside bootstrap, leaving app.js clean for global application specific logics eg(master control)

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,21 +1,13 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
-require('./semantic')
-
+require('./bootstrap') // bootstrap load our application wide dependencies
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ *
+ * This is where global application code should be
+ * this file should contains scripts that
+ * have strong reasons to be made available to every page as general logic/ convenient utilities
  */
-/*
-Vue.component('example', require('./components/Example.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
-*/
