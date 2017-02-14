2f4e303042508945ab048e4e37e43bb89dea3a65
Kenan <xinkenan.11@hotmail.com>
2017-02-15 01:40:55 +0800

    show titles in individual page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>@yield('title')</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
         <link rel="icon" type="image/ico" href="">
 
         <!-- Font Awesome -->
