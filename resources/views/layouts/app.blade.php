18c8d3c1aba9f6cabca75f1e118c51a22aba4b33
Kenan <xinkenan.11@hotmail.com>
2017-02-15 01:33:49 +0800

    removed links
    1. not using bootstrap
    2. jquery is bundled , for now, should consider using cloudflare as heroku server is not in singapore ?
    3. semantic is compiled locally from source

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,11 +37,5 @@
             <p>&copy; {!! date('Y'); !!} <a href="">xxx</a></p>
         </div>
 
-        @yield('pre-javascript')
-        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.8/semantic.min.js"></script>
-        <!-- should we delete this bootstrap? -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-        @yield('javascript')
     </body>
 </html>
