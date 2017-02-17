03bf05f63d27dc29b2fb6477f8ccebd49ec99e76
Teddy Hartanto <hartantoteddy@yahoo.com>
2017-02-17 10:01:05 +0800

    Refactored dashboard related routes to DashboardController

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard.index');
-});
-
 Auth::routes();
+
+Route::get('/dashboard', 'DashboardController@index');
