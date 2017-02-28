84fcc765ee298a50f075c7461e6e48536b6ab77a
dev-seahouse <xinkenan.11@hotmail.com>
2017-03-01 07:52:47 +0800

    :exclamation: add themes

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -26,21 +26,7 @@
                 @endif
 
                 @foreach($user_record_issuers as $record_issuer)
-                    <div class="four wide column">
-                        <div class="dotted-container">
-
-                            <form method="POST" action="{{ url('/dashboard/record_issuers/' . $record_issuer->id) }}" style="display: inline;">
-                                {{ csrf_field() }}
-                                {{ method_field('DELETE') }}
-                                <button type="submit" class="circular red ui icon right button">
-                                    <i class="remove icon"></i>
-                                </button>
-                            </form>
-
-                            <p><a href="{{ route('show_record_issuer', $record_issuer) }}">{{ $record_issuer->name }}</a></p>
-
-                        </div>
-                    </div>
+                    @include('partials.billorgEntry')
                 @endforeach
 
                 <div class="four wide column">
