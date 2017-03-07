e39012bcbed33c2919e36d83d2a5ad665cc453b6
Yan Ling <A0114391@nus.edu.sg>
2017-03-07 15:30:28 +0800

    gets coordinates relative to bill image

--- resources/views/dashboard/editrecord.blade.php
+++ resources/views/dashboard/editrecord.blade.php
@@ -19,28 +19,29 @@
                 </div>
                 
                 <div class="eight wide column">
-                    <div class="dotted-container">
-                        Preview image here
+                    <div class="bill-image">
+                        <img src="{{url('placeholderbill.jpg')}}" style="width:100%;" id="bill" onmousedown="getCoordinates(event)">
                     </div>
                 </div>
                 
                 <div class="eight wide column">
+                    <div class="ui message" id="temp">for test</div>
                     <div class="ui form">
                         <div class="field">
                             <label>Issue Date</label>
-                            <input type="date" name="first-name" placeholder="First Name">
+                            <input type="text" name="issuedate" placeholder="Issue Date">
                         </div>
                         <div class="field">
                             <label>Record Period</label>
-                            <input type="month" name="last-name" placeholder="Last Name">
+                            <input type="text" name="recordperiod" placeholder="Record Period">
                         </div>
                         <div class="field">
                             <label>Due Date</label>
-                            <input type="date" name="last-name" placeholder="Last Name">
+                            <input type="text" name="duedate" placeholder="Due Date">
                         </div>
                         <div class="field">
                             <label>Amount Due</label>
-                            <input type="number" name="amt-due" placeholder="e.g 400">
+                            <input type="text" name="amt-due" placeholder="e.g 400">
                         </div>
                         <button class="ui positive button" type="submit">Submit</button>
                         <button class="ui black button" type="cancel">Cancel</button>
@@ -48,4 +49,52 @@
                 </div>
             </div>
 </div>
+
+<script type="text/javascript">
+    
+    //var billimg = document.getElementById('bill');
+    //billimg.onmousedown = test;
+    
+    function test(e){
+        var content = e.pageX;
+        document.getElementById('temp').innerHTML = content;
+    }
+    
+    function FindPosition(oElement)
+    {
+        if(typeof( oElement.offsetParent ) != "undefined") {
+            for(var posX = 0, posY = 0; oElement; oElement = oElement.offsetParent) {
+                posX += oElement.offsetLeft;
+                posY += oElement.offsetTop;
+            }
+            return [ posX, posY ];
+        }
+        else {
+            return [ oElement.x, oElement.y ];
+        }
+    }
+    
+    function getCoordinates(e){
+        var PosX = 0;
+        var PosY = 0;
+        var ImgPos;
+        var myImg = document.getElementById('bill');
+        ImgPos = FindPosition(myImg);
+        if (!e) var e = window.event;
+        if (e.pageX || e.pageY) {
+            PosX = e.pageX;
+            PosY = e.pageY;
+        }
+        else if (e.clientX || e.clientY) {
+            PosX = e.clientX + document.body.scrollLeft
+                + document.documentElement.scrollLeft;
+            PosY = e.clientY + document.body.scrollTop
+                + document.documentElement.scrollTop;
+        }
+        PosX = PosX - ImgPos[0];
+        PosY = PosY - ImgPos[1];
+        document.getElementById("temp").innerHTML = PosX + ";" + PosY;
+    }
+
+</script>
 @endsection
