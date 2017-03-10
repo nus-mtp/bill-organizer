6eaaf5ef61e9d5749089bdd90d084270326c1753
Yan Ling <A0114391@nus.edu.sg>
2017-03-10 14:04:29 +0800

    click-drag over bill image now can get rectangle coordinates; adjusts coordinates to fit within image even if dragged out of image bounds;

--- resources/views/dashboard/editrecord.blade.php
+++ resources/views/dashboard/editrecord.blade.php
@@ -20,28 +20,29 @@
                 
                 <div class="eight wide column">
                     <div class="bill-image">
-                        <img src="{{url('placeholderbill.jpg')}}" style="width:100%;" id="bill" onmousedown="getCoordinates(event)">
+                        <img src="{{url('placeholderbill.jpg')}}" style="width:100%;" id="bill" onmousedown="getCoordinates(event)" onmouseup="getCoordsAgain(event)" onmouseout="coordsFailSafe(event)">
                     </div>
                 </div>
                 
                 <div class="eight wide column">
+                    for testing
                     <div class="ui message" id="temp">for test</div>
                     <div class="ui form">
                         <div class="field">
                             <label>Issue Date</label>
-                            <input type="text" name="issuedate" placeholder="Issue Date" id="issue" value="what">
+                            <input type="text" name="issuedate" placeholder="Issue Date" id="issue">
                         </div>
                         <div class="field">
                             <label>Record Period</label>
-                            <input type="text" name="recordperiod" placeholder="Record Period">
+                            <input type="text" name="recordperiod" placeholder="Record Period" id="period">
                         </div>
                         <div class="field">
                             <label>Due Date</label>
-                            <input type="text" name="duedate" placeholder="Due Date">
+                            <input type="text" name="duedate" placeholder="Due Date" id="duedate">
                         </div>
                         <div class="field">
                             <label>Amount Due</label>
-                            <input type="text" name="amt-due" placeholder="e.g 400">
+                            <input type="text" name="amt-due" placeholder="e.g 400" id="amtdue">
                         </div>
                         <button class="ui positive button" type="submit">Submit</button>
                         <button class="ui black button" type="cancel">Cancel</button>
@@ -51,6 +52,17 @@
 </div>
 
 <script type="text/javascript">
+    
+    // disable default image drag action so you can drag select box later
+    document.getElementById('bill').ondragstart = function(event) { event.preventDefault(); };
+    
+    var issueDateC;
+    var recPeriodC;
+    var dueDateC;
+    var amtDueC;
+    var billImg = document.getElementById('bill');
+    var tempActField;
+    var selecting = false;
     
     function FindPosition(oElement)
     {
@@ -66,12 +78,11 @@
         }
     }
     
+    var ImgPos = FindPosition(billImg);
+    
     function getCoordinates(e){
         var PosX = 0;
         var PosY = 0;
-        var ImgPos;
-        var billImg = document.getElementById('bill');
-        ImgPos = FindPosition(billImg);
         if (!e) var e = window.event;
         if (e.pageX || e.pageY) {
             PosX = e.pageX;
@@ -85,8 +96,124 @@
         }
         PosX = PosX - ImgPos[0];
         PosY = PosY - ImgPos[1];
-        document.getElementById("temp").innerHTML = PosX + ", " + PosY;
-        document.activeElement.value = PosX + ", " + PosY;
+        var activeField = document.activeElement;
+        tempActField = activeField;
+        activeField.value = PosX + ", " + PosY;
+        
+        if(document.activeElement.id == 'issue'){
+            issueDateC = [PosX, PosY];
+            document.getElementById("temp").innerHTML = "Issue Date: " + issueDateC[0] + ", " + issueDateC[1];
+        }
+        else if(document.activeElement.id == 'period'){
+            recPeriodC = [PosX, PosY];
+            document.getElementById("temp").innerHTML = "Record Period: " + recPeriodC[0] + ", " + recPeriodC[1];
+        }
+        else if(document.activeElement.id == 'duedate'){
+            dueDateC = [PosX, PosY];
+            document.getElementById("temp").innerHTML = "Due Date: " + dueDateC[0] + ", " + dueDateC[1];
+        }
+        else if(document.activeElement.id == 'amtdue'){
+            amtDueC = [PosX, PosY];
+            document.getElementById("temp").innerHTML = "Amount Due: " + amtDueC[0] + ", " + amtDueC[1];
+        }
+        selecting = true;
+    }
+    
+    function getCoordsAgain(e){
+        var PosX = 0;
+        var PosY = 0;
+        if (!e) var e = window.event;
+        if (e.pageX || e.pageY) {
+            PosX = e.pageX;
+            PosY = e.pageY;
+        }
+        else if (e.clientX || e.clientY) {
+            PosX = e.clientX + document.body.scrollLeft
+                + document.documentElement.scrollLeft;
+            PosY = e.clientY + document.body.scrollTop
+                + document.documentElement.scrollTop;
+        }
+        PosX = PosX - ImgPos[0];
+        PosY = PosY - ImgPos[1];
+        
+        if(tempActField.id == 'issue'){
+            issueDateC = issueDateC.concat([PosX, PosY]);
+            document.getElementById("temp").innerHTML = "Issue Date: " + issueDateC;
+        }
+        else if(tempActField.id == 'period'){
+            recPeriodC = recPeriodC.concat([PosX, PosY]);
+            document.getElementById("temp").innerHTML = "Record Period: " + recPeriodC;
+        }
+        else if(tempActField.id == 'duedate'){
+            dueDateC = dueDateC.concat([PosX, PosY]);
+            document.getElementById("temp").innerHTML = "Due Date: " + dueDateC;
+        }
+        else if(tempActField.id == 'amtdue'){
+            amtDueC = amtDueC.concat([PosX, PosY]);
+            document.getElementById("temp").innerHTML = "Amount Due: " + amtDueC;
+        }
+        else{
+            document.getElementById("temp").innerHTML = "nothing selected";
+        }
+        selecting = false;
+    }
+    
+    function coordsFailSafe(e){        
+        if(selecting){
+            var PosX = 0;
+            var PosY = 0;
+            if (!e) var e = window.event;
+            if (e.pageX || e.pageY) {
+                PosX = e.pageX;
+                PosY = e.pageY;
+            }
+            else if (e.clientX || e.clientY) {
+                PosX = e.clientX + document.body.scrollLeft
+                    + document.documentElement.scrollLeft;
+                PosY = e.clientY + document.body.scrollTop
+                    + document.documentElement.scrollTop;
+            }
+            PosX = PosX - ImgPos[0];
+            PosY = PosY - ImgPos[1];
+            
+            var billsize = [document.getElementById('bill').width, document.getElementById('bill').height];
+            
+            //adjust coordinates to fit in image
+            if(PosX > billsize[0]) {
+                PosX = billsize[0]; 
+            }
+            if(PosY > billsize[1]) {
+               PosY = billsize[1];
+            }
+            else if(PosX < 0) {
+                PosX = 0;
+            }
+            else if(PosY < 0) {
+                PosY = 0;
+            }
+            
+            if(tempActField.id == 'issue'){
+                issueDateC = issueDateC.concat([PosX, PosY]);
+                document.getElementById("temp").innerHTML = "Image size: " + billsize +
+                                                        "<br>" + "Issue Date: " + issueDateC;
+            }
+            else if(tempActField.id == 'period'){
+                recPeriodC = recPeriodC.concat([PosX, PosY]);
+                document.getElementById("temp").innerHTML = "Record Period: " + recPeriodC;
+            }
+            else if(tempActField.id == 'duedate'){
+                dueDateC = dueDateC.concat([PosX, PosY]);
+                document.getElementById("temp").innerHTML = "Due Date: " + dueDateC;
+            }
+            else if(tempActField.id == 'amtdue'){
+                amtDueC = amtDueC.concat([PosX, PosY]);
+                document.getElementById("temp").innerHTML = "Amount Due: " + amtDueC;
+            }
+            
+            //document.getElementById('temp').innerHTML = "Image size: " + billsize +
+            //                                            "<br>" + "mouseupped outside image at: " + PosX + ", " + PosY;
+        }
+        selecting = false;
     }
 
 </script>
