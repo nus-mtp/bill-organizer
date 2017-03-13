7a3175ec18b20407b3a4c984d6fd1a6fcba8cb38
dev-seahouse <xinkenan.11@hotmail.con>
2017-03-13 11:37:59 +0800

    add stats controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,9 +43,9 @@
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/headroom/0.9.3/headroom.min.js"></script>
         @yield('pre-javascript')
-            <script src="js/manifest.js"></script>
-            <script src="js/vendor.js"></script>
-            <script src="js/app.js"></script>
+            <script src="/js/manifest.js"></script>
+            <script src="/js/vendor.js"></script>
+            <script src="/js/app.js"></script>
         @yield('javascript')
          <!-- Dump all dynamic scripts into template -->
         @stack('module_scripts')
