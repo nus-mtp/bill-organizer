78b954233ec3ceb8a179ff3a3aaf71321903fc6d
Teddy Hartanto <hartantoteddy@yahoo.com>
2017-03-16 23:58:36 +0800

    Relating RecordIssuer to Template and added tests

--- app/RecordIssuer.php
+++ app/RecordIssuer.php
@@ -19,6 +19,24 @@ class RecordIssuer extends Model
 
     public function issuer_type()
     {
-      return $this->belongsTo(RecordIssuerType::class,'type');
+      return $this->belongsTo(RecordIssuerType::class, 'type');
+    }
+
+    /**
+     * A RecordIssuer has many templates considering that it can update the template of the records it issues
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function templates()
+    {
+        return $this->hasMany(Template::class);
+    }
+
+    /**
+     * Get the latest defined template
+     */
+    public function latest_template()
+    {
+        return $this->templates()->orderBy('created_at', 'desc')->first();
     }
 }
---
+++ tests/Unit/RecordIssuerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Tests\Support\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\Record;
+use App\RecordIssuer;
+use App\Template;
+use App\User;
+
+class RecordIssuerTest extends TestCase
+{
+    use DatabaseMigrations;
+    use DatabaseTransactions;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->runDatabaseMigrations();
+
+        $user = factory(User::class)->create();
+        $this->record_issuer = factory(RecordIssuer::class)->create([
+            'user_id' => $user->id
+        ]);
+        factory(Record::class, 3)->create([
+            'record_issuer_id' => $this->record_issuer->id,
+            'user_id' => $user->id
+        ]);
+        factory(Template::class)->create([
+            'record_issuer_id' => $this->record_issuer->id
+        ]);
+    }
+
+    public function testGetUser()
+    {
+        $this->assertNotNull($this->record_issuer->user);
+    }
+
+    public function testGetRecords()
+    {
+        $this->assertNotNull($this->record_issuer->records);
+    }
+
+    public function testGetIssuerType()
+    {
+        $this->assertNotNull($this->record_issuer->issuer_type);
+    }
+
+    public function testGetTemplates()
+    {
+        $this->assertNotNull($this->record_issuer->templates);
+    }
+
+    public function testGetLatestTemplate()
+    {
+        $this->assertNotNull($this->record_issuer->latest_template());
+    }
+}
