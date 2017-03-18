b23b5956b9bd5972617b95ee2387147e513b3ba2
Yan Ling <A0114391@nus.edu.sg>
2017-03-18 14:05:58 +0800

    beautify dashboard, start to add modal for delete confirmation

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -141,6 +141,22 @@ $(function () {
     }).modal('show')
   })
   
+$('.del-bill-org.button').click(_ => {
+    $('.ui.modal.record-issuer-del-cfm').modal({
+      onApprove: function () {
+          $('.ui.form').submit();
+          // need to return false to not close modal
+          // in case input failed the validation test
+          return false;
+      },
+      onSuccess: function () {
+          $('form#add-record-issuer').submit();
+          $('.modal').modal('hide');
+      }
+    }).modal('show')
+})
+  
+  
   $('.ui.form.record-issuer')
         .form({
         fields: {
--- resources/views/partials/billorgEntry.blade.php
+++ resources/views/partials/billorgEntry.blade.php
@@ -1,16 +1,33 @@
-<div class="four wide column">
-    <div class="dotted-container">
-
+<a class="green card" href="{{ route('show_record_issuer', $record_issuer) }}">
+    <div class="content" style="text-align:center;">
+        <p>{{ $record_issuer->name }}</p>
+    </div>
+    <div class="extra content">
         <form method="POST" action="{{ url('/dashboard/record_issuers/' . $record_issuer->id) }}"
               style="display: inline;">
             {{ csrf_field() }}
             {{ method_field('DELETE') }}
-            <button type="submit" class="circular red ui icon right button">
-                <i class="remove icon"></i>
+            <button class="mini inverted red compact ui icon right floated del-bill-org button">
+                <i class="trash icon"></i>
             </button>
         </form>
+    </div>
+</a>
 
-        <p><a href="{{ route('show_record_issuer', $record_issuer) }}">{{ $record_issuer->name }}</a></p>
-
+<div class="ui small record-issuer-del-cfm modal">
+    <i class="close icon"></i>
+        <div class="content">
+            <form method="POST" action="{{ url('/dashboard/record_issuers/' . $record_issuer->id) }}"
+              style="display: inline;">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit" class="mini inverted red compact ui icon right floated button">
+                <i class="trash icon"></i>
+            </button>
+        </form>
+            <div class="actions">
+            <div class="ui button approve green" data-value="yes">Add</div>
+            <div class="ui button black cancel" data-value="no" onclick="$('form').form('reset'); $('.form .message').html('');">Cancel</div>
+        </div>
     </div>
 </div>
