501aa56f7931f4e927dfe5436fac58349bb0426d
dev-seahouse <xinkenan.11@hotmail.com>
2017-03-19 19:07:02 +0800

    add recordIssuer unit test

--- tests/Unit/RecordIssuerTest.php
+++ tests/Unit/RecordIssuerTest.php
@@ -23,34 +23,36 @@ class RecordIssuerTest extends TestCase{
     public function setUp() {
         parent::setUp();
         $this->prepareDbForTests();
-        $this->user = $this->generateUsersInDatabase()[0];
+        $this->user = $this->generateUserInDb();
     }
 
     public function testCanCreateEmptyRecordIssuerClass(){
        $this->assertInstanceOf(RecordIssuer::class, new RecordIssuer());
     }
 
-    public function testCanSaveRecordIssuerInDatabase() {
-        $recordIssuer = $this->createRecordIssuerModel(self::FIRST_EXAMPLE_RECORD_ISSUER_NAME);
+    public function testCanSaveRecordIssuerInDb() {
+        $recordIssuer = $this->makeRecordIssuerWoFactory(self::FIRST_EXAMPLE_RECORD_ISSUER_NAME);
         $this->user->record_issuers()->save($recordIssuer);
         self::assertTrue($recordIssuer->exists);
     }
 
     public function testCanCreateABillingOrganizationInDbUsingFactory(){
-        $org = $this->generateBillOrgs($this->user,1)[0];
+        $org = $this->createRandBillOrg($this->user);
         self::assertTrue($org->exists);
     }
 
     public function testCanCreateBankStatementOrgTypeInDbUsingFactory(){
-        $org = $this->generateBanks($this->user, 1)[0];
+        $org = $this->createRandStatementIssuer($this->user);
         self::assertTrue($org->exists);
     }
 
-    public function testCreatedOrganizationBelongToCorrectUser(){
-        $recordIssuer = $this->createRecordIssuerModel(self::FIRST_EXAMPLE_RECORD_ISSUER_NAME);
+    public function testCreatedOrganizationShouldBelongToCorrectUser(){
+        $recordIssuer = $this->makeRecordIssuerWoFactory(self::FIRST_EXAMPLE_RECORD_ISSUER_NAME);
         $this->user->record_issuers()->save($recordIssuer);
         $expected = $this->user->id;
         $actual = $recordIssuer->user_id;
         self::assertEquals($expected,$actual);
     }
+
+
 }
