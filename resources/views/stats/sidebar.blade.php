c389e7591fa63cab650903d1e2b675fc2636347e
dev-seahouse <xinkenan.11@hotmail.com>
2017-04-10 15:56:15 +0800

    stats last month

--- resources/views/stats/sidebar.blade.php
+++ resources/views/stats/sidebar.blade.php
@@ -6,7 +6,8 @@
     <form action="/stats/{{$record_issuer->id}}" id="stats-form" method="get">
         <select name= 'stats-options' class="ui fluid s dropdown" id ='js-stats-menu'>
             <option class="item selected" value="0">This Month</option>
-            <option class="item" value="6">past 6 months</option>
+             <option class="item" value="1">Last month</option>
+            <option class="item" value="6">Past 6 months</option>
             <option class="item" value="12">This year</option>
             <option class="item" value="24">Past 2 years</option>
             <option class="item" value="99999">All of time</option>
