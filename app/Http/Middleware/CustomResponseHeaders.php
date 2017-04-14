913c5a8b9650184bbd8be5a93029ad91d210476f
Teddy Hartanto <hartantoteddy@yahoo.com>
2017-04-14 19:29:31 +0800

    Custom response header in response

---
+++ app/Http/Middleware/CustomResponseHeaders.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Response as HttpResponse;
+
+class CustomResponseHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ($response instanceof HttpResponse) {
+            $response->withHeaders([
+                'X-Frame-Options' => 'DENY'
+            ]);
+        }
+
+        return $response;
+    }
+}
